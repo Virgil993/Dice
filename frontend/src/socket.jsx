0ccbb29e669a9bb3e860e4a01e30d248a3cc7107
Virgil993 <turcu.virgil@gmail.com>
2023-05-31 18:12:29 +0300

    Update socket.jsx

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,3 +1,3 @@
 import {io} from 'socket.io-client'
 
-export const socket = io('https://ec2-16-16-146-204.eu-north-1.compute.amazonaws.com:443')
+export const socket = io('https://ec2-16-16-146-204.eu-north-1.compute.amazonaws.com:3000')

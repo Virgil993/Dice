f7a2c8931b6332c1ffe90647ea1a30c3145b5ca9
Virgil993 <turcu.virgil@gmail.com>
2023-06-01 14:15:22 +0300

    Update index.js

--- webSocketsBackend/index.js
+++ webSocketsBackend/index.js
@@ -2,7 +2,7 @@ const httpServer = require('http').createServer();
 
 const io = require("socket.io")(httpServer,{
   cors:{
-    origin:"https://splendid-pithivier-237689.netlify.app/"
+    origin:"*"
   }
 })
 

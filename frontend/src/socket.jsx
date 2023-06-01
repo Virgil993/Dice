f9b63a0df4cb0229c55682d62de34ac751ed8565
Virgil993 <turcu.virgil@gmail.com>
2023-06-01 11:41:23 +0300

    Update socket.jsx

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,3 +1,3 @@
 import {io} from 'socket.io-client'
 
-export const socket = io('https://ec2-16-16-146-204.eu-north-1.compute.amazonaws.com:3000')
+export const socket = io('https://dicegames.ro:3000')

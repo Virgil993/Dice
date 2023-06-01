60882fbcd6a051f919edf34ce9eb07ae9edac655
Virgil993 <turcu.virgil@gmail.com>
2023-06-01 13:09:25 +0300

    Update socket.jsx

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client'
 
 
-fetch("https://16.16.146.204:3000").then(response => response.json()).then(data => {
+fetch("http://dicegames.ro:3000").then(response => response.json()).then(data => {
     console.log("Fetch response")
     console.log(data)
 }).catch(err => {
@@ -9,4 +9,4 @@ fetch("https://16.16.146.204:3000").then(response => response.json()).then(data 
     console.log(err)
 })
 
-export const socket = io('https://16.16.146.204:3000')
+export const socket = io('http://dicegames.ro:3000')

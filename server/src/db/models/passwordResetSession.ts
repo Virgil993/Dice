d2da7bc48bd3551313b4115d7e3952c1fe916d5f
Virgil993 <turcu.virgil@gmail.com>
2025-05-09 15:46:05 +0300

    auth models initialized

---
+++ server/src/db/models/passwordResetSession.ts
@@ -0,0 +1,73 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  Sequelize,
+} from "sequelize";
+
+export class PasswordResetSession extends Model<
+  InferAttributes<PasswordResetSession>,
+  InferCreationAttributes<PasswordResetSession>
+> {
+  declare id: CreationOptional<string>;
+  declare userId: string;
+  declare token: string;
+  declare usedAt: CreationOptional<Date | null>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+  declare deletedAt: CreationOptional<Date | null>;
+}
+
+export const initPasswordResetSessionModel = (db: Sequelize): void => {
+  PasswordResetSession.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.STRING(512),
+        allowNull: false,
+        unique: true,
+      },
+      usedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    },
+    {
+      sequelize: db,
+      tableName: "password_reset_sessions",
+      modelName: "PasswordResetSession",
+      paranoid: true,
+      underscored: true,
+      indexes: [
+        {
+          name: "idx_password_reset_sessions_deleted_at",
+          fields: ["deleted_at"],
+        },
+      ],
+    }
+  );
+};

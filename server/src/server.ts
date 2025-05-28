ebc535cb32c1b53df6f66e3490ffeeb1b0f02ae8
Virgil993 <turcu.virgil@gmail.com>
2025-05-29 00:10:56 +0300

    wip messages page frontend

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import { createApp } from "./app";
 import http from "http";
 import { RDSAuthManager } from "./db/rdsAuth";
 import { loadSecrets } from "./config/secrets";
+import { RedisInstance } from "./config/redis";
+import { initializeWebSocket } from "./websockets/initialize";
 
 dotenv.config();
 
@@ -10,15 +12,18 @@ async function startServer() {
   // Initialize the database connection and models
   const secrets = await loadSecrets();
 
+  const redisClient = new RedisInstance(secrets).getClient();
   const PORT = process.env.PORT || 3000;
-  const app = createApp(secrets);
+  const app = createApp(secrets, redisClient);
   const server = http.createServer(app);
+  const wss = initializeWebSocket(server, secrets, redisClient);
 
   const rdsAuthManager = new RDSAuthManager();
   await rdsAuthManager.initialize();
 
   server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log(`websocket available on ws://localhost:${PORT}/ws`);
   });
 
   // Handle uncaught exceptions
@@ -31,6 +36,17 @@ async function startServer() {
   process.on("unhandledRejection", (reason, promise) => {
     console.error("Unhandled Rejection at:", promise, "reason:", reason);
     process.exit(1);
+  });
+
+  // Graceful shutdown
+  process.on("SIGTERM", () => {
+    console.log("SIGTERM received, shutting down gracefully");
+    wss.clients.forEach((ws) => {
+      ws.close(1001, "Server shutting down");
+    });
+    server.close(() => {
+      console.log("Process terminated");
+    });
   });
 }
 

11650de85083be635d3bec9d722dab7a1551af7e
Virgil993 <turcu.virgil@gmail.com>
2025-06-01 23:13:59 +0300

    use wss in prod for frontend

--- client/src/layouts/WebSocket.tsx
+++ client/src/layouts/WebSocket.tsx
@@ -16,8 +16,9 @@ export function WebSocketProvider({ children }: WebSocketProviderProps) {
       return;
     }
     const apiUrl = import.meta.env.VITE_API_BASE_URL;
+    const environment = import.meta.env.VITE_ENVIRONMENT;
     const ws = new WebSocket(
-      `ws://${apiUrl
+      `w${environment === "dev" ? "s" : "ss"}://${apiUrl
         .replace("http://", "")
         .replace("https://", "")}/ws?token=${token}`
     );
